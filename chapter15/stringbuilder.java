7d59b272d54b857aa53c98e94ce5cfd6d80cab64
nakayama91 <k.naka10ry@gmail.com>
2022-02-06 21:17:27 +0900

    ch15-3

---
+++ chapter15/stringbuilder.java
@@ -0,0 +1,10 @@
+//String Builderを利用し文字列を１万回連結する
+public class Main {
+  public static void main (String[] args) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10000; i++) {
+      sb.append("Java");
+    }
+    String s = sb.toString();
+  }
+}
