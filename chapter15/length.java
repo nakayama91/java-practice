f038e6f52bf74a81d0ea7effed46c6afbe7eb8de
nakayama91 <k.naka10ry@gmail.com>
2022-02-07 21:44:39 +0900

    15-4

--- chapter15/length.java
+++ chapter15/length.java
@@ -9,7 +9,7 @@ public class Main {
     if (s2.equalsIgnoreCase(s3)) {
       System.out.println("s2とs3はケースを区別しなければ等しい");
     }
-    System.out.println("s1の長さは" + s1.length()) + "です"); //全角文字も半角文字も１文字としてカウント
+    System.out.println("s1の長さは" + s1.length() + "です"); //全角文字も半角文字も１文字としてカウント
     if (s1.isEmpty()) {
       System.out.println("s1は空文字です");
     }
